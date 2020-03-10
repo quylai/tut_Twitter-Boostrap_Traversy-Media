699c6b1055c8380f3f9f5b3859759bf3bbac73db
quylai <quylai@gmail.com>
2020-03-10 12:57:57 -0500

    created pages.js for pages.html

---
+++ 02/rev-01/js/pages.js
@@ -0,0 +1,10 @@
+
+// add editing tools onto modal (from {Create Content / Add Page})
+CKEDITOR.replace( 'editor1' );
+
+// console.log("testingasdf");  // db
+
+
+
+
+//---------------------------------------------------------------------------------
