f3ba75d8ef38e9f6aa88b82d9595b3fb94a940c9
quylai <quylai@gmail.com>
2020-03-10 18:06:59 -0500

    added responsive search and modified delete btn action

--- 02/rev-01/js/pages.js
+++ 02/rev-01/js/pages.js
@@ -4,10 +4,39 @@ CKEDITOR.replace("editor1");
 
 // delete bnt functionality
 document.querySelector("#pages-list").addEventListener("click", e => {
-  let pagesList = e.target.parentNode.parentNode.parentNode;
-  pagesList.removeChild(e.target.parentNode.parentNode);
+
+  if(e.target.classList.contains("btn-danger")) {
+    let pagesList = e.target.parentNode.parentNode.parentNode;
+    pagesList.removeChild(e.target.parentNode.parentNode);
+  }
 });
 
+// ----------------------------------------------------------------
 // responsive search functionality
+
+// latch to DOM and trigger search function when typing
+let filterInput = document.getElementById("searching");
+filterInput.addEventListener("keyup", filterPages);
+
+// traversing thru list
+function filterPages(){
+
+  // turn input value to lowercase
+  let filterValue = document.getElementById("searching").value.toLowerCase();
+
+  // gather all element of page-list titles
+  let pagesList = document.getElementById('pages-list')
+                          .querySelectorAll("td.page-title");
+
+  // iterate and hide those NOT match
+  for(const el of pagesList) {
+    if(el.innerHTML.toLowerCase().indexOf(filterValue) > -1) {
+      el.parentNode.style.display = "";
+    } else {
+      el.parentNode.style.display = "none";
+    }
+  }
+}
+// ----------------------------------------------------------------
 
 //---------------------------------------------------------------------------------
