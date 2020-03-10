cd75bb2663370a90cc38e0c36c4201c877a0a028
quylai <quylai@gmail.com>
2020-03-10 15:40:01 -0500

    done

--- 02/rev-01/js/pages.js
+++ 02/rev-01/js/pages.js
@@ -1,10 +1,12 @@
 
 // add editing tools onto modal (from {Create Content / Add Page})
-CKEDITOR.replace( 'editor1' );
+CKEDITOR.replace("editor1");
 
-// console.log("testingasdf");  // db
-
-
+// delete bnt functionality
+document.querySelector("#pages-list").addEventListener("click", e => {
+  let pagesList = e.target.parentNode.parentNode.parentNode;
+  pagesList.removeChild(e.target.parentNode.parentNode);
+});
 
 
 //---------------------------------------------------------------------------------
